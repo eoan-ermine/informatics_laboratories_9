710ae07caf5eab1f841960708df40a4a5aef14ef
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-08 09:57:12 +0300

    Implement view for static_handler.html

--- first_web_page/urls.py
+++ first_web_page/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('hello/', views.home, name='hello'),
+    path("cute_page/", views.cute_page, name="cute_page")
 ]
--- flatpages/views.py
+++ flatpages/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request, "index.html")
+
+
+def cute_page(request):
+	return render(request, "static_handler.html")
